87d0f4a8032f3cf6e207a50e9d581a763146af15
Azure Release Agent for SimpleITK.github.io <simpleitk_github_io@itk.org>
2021-10-29 07:11:15 +0000

    Doxygen latest updated
    built from c234892ecc3472814905c976b7e39ffe915a36bf

--- doxygen/latest/html/menudata.js
+++ doxygen/latest/html/menudata.js
@@ -78,6 +78,7 @@ var menudata={children:[
 {text:"x",url:"namespacemembers_func_x.html#index_x"},
 {text:"y",url:"namespacemembers_func_y.html#index_y"},
 {text:"z",url:"namespacemembers_func_z.html#index_z"}]},
+{text:"Variables",url:"namespacemembers_vars.html"},
 {text:"Typedefs",url:"namespacemembers_type.html"},
 {text:"Enumerations",url:"namespacemembers_enum.html"},
 {text:"Enumerator",url:"namespacemembers_eval.html",children:[
