a93caab886bc382ac090757483e24ac547d54f98
Azure Release Agent for SimpleITK.github.io <simpleitk_github_io@itk.org>
2023-01-11 07:35:38 +0000

    Doxygen latest updated
    built from 4e73de125c853f9479dc9d3c07e0adb4c662427b

--- doxygen/latest/html/build_text.js
+++ doxygen/latest/html/build_text.js
@@ -1,4 +1,4 @@
 $(function(){
-    document.getElementById("datetime").textContent = "Tue, 10 Jan 2023 03:50:05 +0000"
+    document.getElementById("datetime").textContent = "Wed, 11 Jan 2023 03:58:22 +0000"
     document.getElementById("projectnumber").textContent = "2.3.0.dev"
 });
