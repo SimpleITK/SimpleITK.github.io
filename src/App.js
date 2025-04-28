1812244092466ec223b82ef947f8bd3ce5451014
Bradley Lowekamp <blowekamp@mail.nih.gov>
2025-04-28 19:47:39 -0400

    Add featurettes to Hero component and update package.json
    * **Hero component**: Add featurettes array with imageSrc, description, and reverse properties. Update return statement to include featurettes mapping.
    * **App.js**: Remove import and usage of Featurettes component. Update routing to include Hero component.
    * **package.json**: Add react-router-dom dependency.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavbarComponent from './components/Navbar';
 import Hero from './components/Hero';
-import Featurettes from './components/Featurette';
 import Footer from './components/Footer';
 import About from './components/About';
 import PrivacyPolicy from './components/PrivacyPolicy';
@@ -17,7 +16,6 @@ const App = () => {
           <Route path="/about" component={About} />
           <Route path="/privacy-policy" component={PrivacyPolicy} />
         </Switch>
-        <Featurettes />
         <Footer />
       </div>
     </Router>
